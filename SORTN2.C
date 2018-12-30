2c983bb366431b191a39e042cbe01691d356dff9
BLINA4 <shayda.andrey@yandex.ru>
2018-12-31 01:51:47 +0300

    Added some templates for O(N^2) sort functions

---
+++ SORTN2.C
@@ -0,0 +1,32 @@
+/* Sorting algorithms programm
+ * N^2 Sort functions file 
+ * Protected by GNU GPL v3 license */
+
+#include "SORT.H"
+
+/* Bubble sort function */
+void BubbleSort( int *Mas, int N )
+{
+
+} /* End of 'BubbleSort' function */
+
+/* Bubble sort (upgraded) function */
+void BubbleSort1( int *Mas, int N )
+{
+  
+} /* End of 'BubbleSort1' function */
+
+/* Insertion sort function */
+void InsertionSort( int *Mas, int N )
+{
+
+} /* End of 'InsertionSort' function */
+
+/* Selection sort function */
+void SelectionSort( int *Mas, int N )
+{
+
+} /* End of 'SelectionSort' function */   
+
+/* END OF 'SORTN2.C' FILE */
+
---
+++ UTILS.C
@@ -0,0 +1,18 @@
+/* Sorting algorithms programm
+ * Utilities function file
+ * Protected by GNU GPL license v3 */
+
+#include "SORT.H"
+
+/* Integer swap function */
+void Swap( int *X, int *Y )
+{
+  int *tmp;
+
+  tmp = *X;
+  *X = *Y;
+  *Y = tmp;
+} /* End of 'Swap' function */
+
+/* END OF 'UTILS.C' FILE */
+
