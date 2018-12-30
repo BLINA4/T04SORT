48811ab0a24c34ec8b49711ed3825209c20f25e7
BLINA4 <shayda.andrey@yandex.ru>
2018-12-31 01:22:21 +0300

    Added main file and 'main' function

---
+++ T04SORT.C
@@ -0,0 +1,12 @@
+/* Sorting algorithms programm
+ * Main programm file
+ * Protected by GNU GPL license v3 */
+
+/* Main programm function */
+void main( void )
+{
+
+} /* End of 'main' function */
+
+/* END OF 'T04SORT.C' FILE */
+
